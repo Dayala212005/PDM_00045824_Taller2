e5691ee91e26d73b3ab9f0e732beb6ea2488d5ac
Dayala212005 <00045824@uca.edu.sv>
2026-05-13 22:30:56 -0600

    feat: Agregar Navegacion

--- app/src/main/java/com/pdm0126/foodspot/navigation/AppNavigation.kt
+++ app/src/main/java/com/pdm0126/foodspot/navigation/AppNavigation.kt
@@ -1,2 +1,27 @@
 package com.pdm0126.foodspot.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+
+@Composable
+fun FoodSpot() {
+    val backStack = rememberNavBackStack(Routes.RestauranteListas)
+
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryProvider = entryProvider{
+            entry<Routes.RestauranteListas> {
+
+            }
+            entry<Routes.RestauranteDetail> {
+
+            }
+            entry<Routes.BusquedaGeneral> {
+
+            }
+        }
+    )
+}
--- app/src/main/java/com/pdm0126/foodspot/navigation/Routes.kt
+++ app/src/main/java/com/pdm0126/foodspot/navigation/Routes.kt
@@ -1,4 +1,13 @@
 package com.pdm0126.foodspot.navigation
 
-class Routes {
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed class Routes: NavKey{
+    @Serializable
+    data object RestauranteListas: Routes()
+    @Serializable
+    data class RestauranteDetail(val id: Int): Routes()
+    @Serializable
+    data object BusquedaGeneral: Routes()
 }
